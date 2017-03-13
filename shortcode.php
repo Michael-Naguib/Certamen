a867ed342a986e4c09991b86fbee23e8fdbb435f
Michael-N <soccer72msn2@gmail.com>
2017-03-13 17:14:01 -0400

    Header work for index.html & reminder comment in shortcode.php

--- shortcode.php
+++ shortcode.php
@@ -6,4 +6,5 @@
     $myfile = fopen($filePathShortcodeIndex, "r") or die("[Shortcode] Unable to open ".$filePathShortcodeIndex);
     echo fread($myfile,filesize($filePathShortcodeIndex));
     fclose($myfile);
+    echo "<div style='display:hidden'>READ ME READ ME READ ME READ ME READ ME READ ME ~~ some links may not work depending on the directory from which this script is run, they may need to be adjusted see index.js,html,css, etc....</div>";
 ?>
