5a3bc32827a2aebf639172709187f1468e73082b
Michael-N <soccer72msn2@gmail.com>
2017-03-13 14:39:29 -0400

    workflow streamline enhancements

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,42 @@
 //Requires & Setup
-var gulp = require('gulp');
-var react = require('gulp-react');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const react = require('gulp-react');
+const sass = require('gulp-sass');
+const htmlmin = require("gulp-htmlmin");
+const rename = require("gulp-rename");
 
 //Settings and Configuration
 const settings = {
-    sassInDir:"dev",
-    sassOutDir:"dev",
-    jsxInDir:"",
-    jsxOutDir:""
+    sassInDir:"dev/",
+    sassOutDir:"www/stylesheets/",
+    jsxInDir:"dev/",
+    jsxOutDir:"www/javascript/",
+    minhtmlInDir:"dev",
+    minhtmlOutDir:"www/markup/"
 };
 
 //Manage Sass
 gulp.task("sass",function(){
-    return gulp.src(settings.sassInDir).pipe(sass.sync().on('error', sass.logError)).pipe(gulp.dest(settings.sassOutDir));
+    return gulp.src(settings.sassInDir)
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest(settings.sassOutDir));
 });
 
 //Manage Jsx
 gulp.task("jsx",function(){
-    return gulp.src(settings.jsxInDir).pipe(react()).pipe(gulp.dest(settings.jsxOutDir));
+    return gulp.src(settings.jsxInDir)
+        .pipe(react())
+        .pipe(gulp.dest(settings.jsxOutDir));
+});
+
+// Manage Minify Html
+gulp.task("minhtml",function(){
+    return gulp.src(settings.minhtmlInDir)
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(rename(function(path){
+            path.extname = ".min.html";
+        }))
+        .pipe(gulp.dest(settings.minhtmlOutDir));
 });
 
 //Watch all the files as the default task
@@ -28,4 +46,7 @@ gulp.task("default",function(){
     
     //watch jsx
     gulp.watch(settings.jsxInDir,["jsx"]);
+    
+    // watch html
+    gulp.watch(settings.minhtmlInDir,["minhtml"]);
 });
