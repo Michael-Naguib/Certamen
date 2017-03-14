d68d2c2fd39b5cc8bf3d8f54a906f4807bb5c8b0
Michael N <soccer72msn2@gmail.com>
2017-03-14 12:26:38 -0400

    Update leaderboard.jsx
    Added in basic functionality for query Ajax Request needs debugging and completion.... Try to relieve the constant setState calls.... also implement the request via the main app component!!!--> only request ONCE or a system of retrying.... do not for get the anti spam with timers

--- dev/javascript/leaderboard.jsx
+++ dev/javascript/leaderboard.jsx
@@ -1,7 +1,35 @@
 //Header
 const LeaderboardHeader = <div id="leaderboard-header">Leaderboards</div>;
 
-//
+class LeaderboardAside extends React.Component{
+        constructor(props){
+        super(props);
+        this.state ={
+            userData: undefined,
+            spamed: false,
+            genDataUrl: "./assets/genleaderboards.php",
+            retrievalErr: false
+        };
+        this.getUserData = this.getUserData.bind(this);
+    }
+    getUserData(){
+        this.setState({spammed:true});
+        $.ajax({url:this.state.genDataUrl, success: function(result){
+            try{
+                this.setState({userData:Json.parse(result)});
+            }catch(var e){
+                this.setState({retrievalErr: true});
+            }
+            
+        }, error: function(xhrr,statuss,errorr){
+            this.setState({retrievalErr: true});
+        }});
+    }
+    render(){
+        return(<div id="leaderboard-aside"></div>);
+    }
+}
+//Main Component
 class Leaderboard extends React.Component{
     constructor(props){
         super(props);
